4089184a995e9c4ceff69e78025e98e017cc9461
san <1083105844@qq.com>
2019-12-05 21:20:08 +0800

    first commit

---
+++ src/components/header.vue
@@ -0,0 +1,54 @@
+<template>
+    <van-nav-bar
+            :title="title"
+            left-text=""
+            right-text=""
+            left-arrow
+            @click-left="onClickLeft"
+            @click-right="onClickRight"
+    />
+</template>
+
+<script>
+    import { NavBar } from 'vant';
+
+    export default {
+        name: "Title",
+        components:{
+            [NavBar.name]:NavBar
+        },
+        props : {
+            title : {
+                type : String,
+                default : "标题",
+            }
+        },
+        data(){
+            return{
+
+            }
+        },
+        methods: {
+            onClickLeft() {
+               this.$emit("onClickLeft")
+            },
+            onClickRight() {
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .van-nav-bar__title {
+        max-width: 60%;
+        margin: 0 auto;
+        color: #202020;
+        font-weight: 500;
+        font-size: 16px;
+    }
+
+    .van-icon-arrow-left{
+        color: #9E9E9E;
+    }
+</style>
