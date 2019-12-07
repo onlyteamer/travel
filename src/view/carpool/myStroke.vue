96c2b7379b0d997032f6ebe37e3b4191df8cc387
san <1083105844@qq.com>
2019-12-07 16:04:40 +0800

    1我的行程

---
+++ src/view/carpool/myStroke.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="contain">
+        <Title :title="title" @onClickLeft="onClickLeft"></Title>
+
+    </div>
+</template>
+
+<script>
+    import Title from './../../components/header'
+    import {  Tab, Tabs,Divider,Row, Col,Button   } from 'vant';
+
+    export default {
+        name: "myStroke",
+        components:{
+            Title,
+            [Tab.name]:Tab,
+            [Tabs.name]:Tabs,
+            [Divider.name]:Divider,
+            [Row.name]:Row,
+            [Col.name]:Col,
+            [Button.name]:Button
+        },
+        data(){
+            return{
+                title:"我的行程"
+            }
+        },
+        methods: {
+            onClickLeft(){
+                console.log(11111);
+
+            },
+        }
+    }
+</script>
+
+<style scoped>
+    body{
+        background: #FFFFFF;
+    }
+
+    .contain{
+
+    }
+
+</style>
