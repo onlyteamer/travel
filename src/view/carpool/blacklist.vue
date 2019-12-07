63cdcfda2fef4edd1304248c6c0d78867d1a3ac3
san <1083105844@qq.com>
2019-12-07 19:04:51 +0800

    黑名单

---
+++ src/view/carpool/blacklist.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="contain">
+        <Title :title="title" @onClickLeft="onClickLeft"></Title>
+
+        <div>
+            <div class="black" v-for="index in 6">
+                <van-row style="display: flex;align-items: center">
+                    <van-col span="12" >
+                        <div style="display: flex;align-items: center">
+                            <img src="../../static/images/userAvatar.png" style="height: 50px;width: 50px;margin-right: 10px">
+                            <div>
+                                <p style="margin: 5px 0"><span style="color: #5E5E5E;font-weight: bold">加菲猫</span><img src="../../static/images/sexTag.png" style="width: 12px;height: 12px;margin-left: 5px"> </p>
+                                <p style="margin: 5px 0">
+                                    <img src="./../../static/images/xin.png" style="width: 14px;height: 14px;margin-right: 5px"/><span>231</span>
+                                    <img src="./../../static/images/unhapply.png" style="width: 14px;height: 14px;margin: 0 5px 0 20px" /> <span>3</span>
+                                </p>
+                                <p style="margin: 5px 0">
+                                    <span style="color: #5E5E5E">乘坐2次</span>
+                                </p>
+                            </div>
+                        </div>
+                    </van-col>
+                    <van-col span="12" align="right">
+                        <div><div class="userType">车主</div></div>
+
+                        <div><div class="removeTag">移除黑名单</div></div>
+                    </van-col>
+                </van-row>
+
+
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import Title from './../../components/header'
+    import { Row, Col} from 'vant';
+
+    export default {
+        name: "blacklist",
+        components:{
+            Title,
+            [Row.name]:Row,
+            [Col.name]:Col
+        },
+        data(){
+            return{
+                title:"黑名单"
+            }
+        },
+        methods:{
+            onClickLeft(){
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    .black{
+        width: 90%;
+        margin: 10px auto;
+        padding: 5px 10px;
+        background: #FFFFFF;
+        font-size: 14px;
+    }
+
+    .userType{
+        width: fit-content;
+        padding: 2px 8px;
+        border-radius: 5px;
+        color: #FFFFFF;
+        background: #5083ED;
+    }
+
+    .removeTag{
+        width: fit-content;
+        padding: 2px 8px;
+        margin-top: 10px;
+        color: #0CC893;
+        border-radius: 5px;
+        border: 1px solid #0CC893;
+    }
+</style>
