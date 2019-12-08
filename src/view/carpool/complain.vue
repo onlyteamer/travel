86ae3e278f3efe808c6dc86ee7ec7345ff4618dc
朝暮 <zhaomu0248@163.com>
2019-12-08 10:54:29 +0800

    车主取消行程图标

---
+++ src/view/carpool/complain.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div>
+            <van-nav-bar title="取消行程" :fixed="true" left-arrow @click-left="onClickLeft"/>
+        </div>
+        <div class="content">
+        </div>
+    </div>
+</template>
+
+<script>
+    import {NavBar, Button, RadioGroup, Radio} from 'vant';
+    export default {
+        components: {
+            [NavBar.name]: NavBar,
+            [Button.name]: Button,
+            [RadioGroup.name]: RadioGroup,
+            [Radio.name]: Radio,
+            [Image.name]: Image,
+        },
+        data() {
+            return {
+                radio: ''
+            }
+        },
+        methods: {
+            onClickLeft() {
+                this.$router.back(-1);
+            },
+            submit(){
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
