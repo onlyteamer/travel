1cbd3dd3bce6729aa5d9756b382a9640a1ebb4ec
朝暮 <zhaomu0248@163.com>
2019-12-08 21:16:09 +0800

    通用协议页

---
+++ src/view/agreement/agreement.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div>
+            <van-nav-bar :fixed="true" title="协议" left-arrow @click-left="onClickLeft"/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {NavBar} from 'vant';
+
+    export default {
+        components: {
+            [NavBar.name]: NavBar,
+        },
+        methods: {
+            onClickLeft() {
+                this.$router.back(-1);
+            },
+        }
+    }
+</script>
+
+<style scoped>
+    /deep/ .van-nav-bar .van-icon {
+        color: #5E5E5E
+    }
+
+</style>
