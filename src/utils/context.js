5f6188538dbd29aec79a30be4c385c31746e4488
朝暮 <zhaomu0248@163.com>
2019-12-27 08:57:52 +0800

    修改url

--- src/utils/context.js
+++ src/utils/context.js
@@ -1,13 +1,6 @@
 const context = {};
 context.imageServer='https://gstptravel.oss-cn-zhangjiakou.aliyuncs.com/static/images/';
-context.baseUrl = '';
-{
-    if(location.hostname==='gstp.huntauto.com.cn'){
-        context.baseUrl = 'https://gstp.huntauto.com.cn/api';
-    }else{
-        context.baseUrl ='https://gstpadmin.huntauto.com.cn' ;
-    }
-}
+context.baseUrl ='https://gstpadmin.huntauto.com.cn' ;
 context.pageUrl = 'https://gstp.huntauto.com.cn/wx/index.html';
 context.wxGetConfigUrl='https://gstp.huntauto.com.cn/wx/redirect/wxfe42689b9e435f72/signature?url=https://gstp.huntauto.com.cn/wx/travel/demo.html';
 context.wxGetPosition = 'http://gstpapi.huntauto.com.cn//wx/redirect/wxfe42689b9e435f72/signature?url=http://gstpapi.huntauto.com.cn/wx/travel/demo.html';
