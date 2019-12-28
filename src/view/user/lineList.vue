7bfe7299676cee4a7bef7343360bd9c04791b65e
朝暮 <zhaomu0248@163.com>
2019-12-28 22:46:49 +0800

    切换线路

---
+++ src/view/user/lineList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="content">
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    import { Button, Toast} from 'vant';
+    import request from "../../utils/request"
+    export default {
+        [Button.name]: Button,
+        [Toast.name]: Toast
+    }
+</script>
+
+<style scoped>
+
+</style>
