7dd6d3acc963493508cec2bcc73a0311dbd01a6f
朝暮 <zhaomu0248@163.com>
2020-01-01 10:55:52 +0800

    去掉获取微信签名链接

--- src/utils/context.js
+++ src/utils/context.js
@@ -9,6 +9,4 @@ context.baseUrl ='';
     }
 }
 context.pageUrl = 'https://gstp.huntauto.com.cn/index.html';
-context.wxGetConfigUrl='https://gstp.huntauto.com.cn/wx/redirect/wxfe42689b9e435f72/signature?url=https://gstp.huntauto.com.cn/travel/demo.html';
-context.wxGetPosition = 'http://gstpapi.huntauto.com.cn/wx/redirect/wxfe42689b9e435f72/signature?url=http://gstpapi.huntauto.com.cn/travel/demo.html';
 export default context;
