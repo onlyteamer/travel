8ac1e7dbbd7df0182dbcfdd307ce54a554842e8a
朝暮 <zhaomu0248@163.com>
2020-02-02 00:27:01 +0800

    修改地图

--- src/view/carpool/carOwnPosition.vue
+++ src/view/carpool/carOwnPosition.vue
@@ -1,29 +1,17 @@
 <template>
     <div class="contain">
-<!--        <aMap-->
-<!--                ref="carMap" id="hotelContainer"-->
-<!--                @select="getPosition"-->
-<!--                @getAddress="getAddress"-->
-<!--                :lon="carInfo.lon"-->
-<!--                :lat="carInfo.lat"-->
-<!--                :tripId="tripId"-->
-<!--                placeholder="请输入酒店地址或点击下面地图" height="450px">-->
-<!--        </aMap>-->
-<!--        <div style="text-align: center;padding: 10px;background: #FFFFFF;border-top: 1px solid #ECECEC">{{carPosition.updateTime}}</div>-->
         <div class="amap-wrapper">
-            <el-amap vid="amapDemo">
-<!--                <el-amap-marker vid="component-marker" :position="componentMarker.position" :content-render="componentMarker.contentRender" ></el-amap-marker>-->
-<!--                <el-amap-marker v-for="(marker, index) in markers" :position="marker.position" :events="marker.events" :visible="marker.visible" :draggable="marker.draggable" :vid="index"></el-amap-marker>-->
+            <el-amap vid="amapDemo" :center="center">
+                <el-amap-marker v-for="(marker, index) in markers" :position="marker.position" :visible="marker.visible" :draggable="marker.draggable" :vid="index"></el-amap-marker>
+                <el-amap-text v-for="text in texts" :text="text.text" :offset="text.offset" :position="text.position" ></el-amap-text>
             </el-amap>
         </div>
-<!--        <div>-->
-<!--            <div id="hotelContainer" :style="{'width':'100%','height':'450px'}"></div>-->
-<!--        </div>-->
-
         <div class="footer">
-            <div style="margin-bottom: 10px;display: flex;align-items: center"><van-icon name="warning" color="#F55456"/><span style="font-weight: bold;margin-left: 5px">注意：</span></div>
+            <div style="margin-bottom: 10px;display: flex;align-items: center">
+                <van-icon name="warning" color="#F55456"/>
+                <span style="font-weight: bold;margin-left: 5px">注意：</span></div>
             <div style="margin-bottom: 10px">1. 车主的位置仅供参考；</div>
-            <div >2. 如果车主位置一直未刷新，建议电话联系车主。</div>
+            <div>2. 如果车主位置一直未刷新，建议电话联系车主。</div>
         </div>
 
         <div style="width: 100%">
@@ -40,7 +28,7 @@
 
 <script>
     import Title from './../../components/header'
-    import { Row, Col,Divider,Button,Rate,Tag ,Field,CellGroup,Icon ,Tabbar,TabbarItem} from 'vant';
+    import {Row, Col, Divider, Button, Rate, Tag, Field, CellGroup, Icon, Tabbar, TabbarItem} from 'vant';
 
     import wx from 'weixin-js-sdk'
     import request from '../../utils/request';
@@ -51,123 +39,84 @@
 
     const exampleComponents = {
         props: ['text'],
-        template: `<div>text from  parent: {{text}}</div>`
+        template: `
+            <div>text from parent: {{text}}</div>`
     };
     export default {
         name: "carOwnPosition",
-        components:{
+        components: {
             Title,
             // aMap,
-            [Row.name]:Row,
-            [Col.name]:Col,
-            [Divider.name]:Divider,
-            [Button.name]:Button,
-            [Rate.name]:Rate,
-            [Tag.name]:Tag,
-            [Field.name]:Field,
-            [CellGroup.name]:CellGroup,
-            [Icon.name]:Icon,
+            [Row.name]: Row,
+            [Col.name]: Col,
+            [Divider.name]: Divider,
+            [Button.name]: Button,
+            [Rate.name]: Rate,
+            [Tag.name]: Tag,
+            [Field.name]: Field,
+            [CellGroup.name]: CellGroup,
+            [Icon.name]: Icon,
             [Tabbar.name]: Tabbar,
             [TabbarItem.name]: TabbarItem
         },
-        data(){
-            return{
-                active:"",
-                title:"车主位置",
-                passengerPosition:{
-                    lat:"39.865042",
-                    lon:"116.379028"
+        data() {
+            return {
+                active: "",
+                title: "车主位置",
+                passengerPosition: {
+                    lat: "39.865042",
+                    lon: "116.379028"
                 },
-                driverPosition:{
-                    lat:"39.865042",
-                    lon:"116.379028"
+                driverPosition: {
+                    lat: "39.865042",
+                    lon: "116.379028"
                 },
-                tripId:"",
+                tripId: "",
                 chengK: chengK,
                 xingC: xingC,
                 push: push,
                 person: person,
-                count: 1,
                 slotStyle: {
                     padding: '2px 8px',
                     background: '#eee',
                     color: '#333',
                     border: '1px solid #aaa'
                 },
-                zoom: 14,
-                center: [121.5273285, 31.21515044],
-                markers: [
-                    {
-                        position: [121.5273285, 31.21515044],
-                        events: {
-                            click: () => {
-                                alert('click marker');
-                            },
-                            dragend: (e) => {
-                                console.log('---event---: dragend')
-                                this.markers[0].position = [e.lnglat.lng, e.lnglat.lat];
-                            }
-                        },
-                        visible: true,
-                        draggable: false,
-                        template: '<span>1</span>',
-                    }
-                ],
-                renderMarker: {
-                    position: [121.5273285, 31.21715058],
-                    contentRender: (h, instance) => {
-                        return h(
-                            'div',
-                            {
-                                style: {background: '#80cbc4', whiteSpace: 'nowrap', border: 'solid #ddd 1px', color: '#f00'},
-                                on: {
-                                    click: () => {
-                                        const position = this.renderMarker.position;
-                                        this.renderMarker.position = [position[0] + 0.002, position[1] - 0.002];
-                                    }
-                                }
-                            },
-                            ['marker inner text']
-                        )
-                    }
-                },
-                componentMarker: {
-                    position: [121.5273285, 31.21315058],
-                    contentRender: (h, instance) => h(exampleComponents,{style: {backgroundColor: '#fff'}, props: {text: 'father is here'}}, ['xxxxxxx'])
-                },
-                slotMarker: {
-                    position: [121.5073285, 31.21715058]
-                }
+                zoom: 13,
+                center: [116.403912,39.894243],
+                markers: [],
+                texts: [],
             }
         },
-        created(){
+        created() {
             this.tripId = this.$route.query.tripId;
         },
 
-        mounted(){
+        mounted() {
             this.getOwnerPosition();
             this.getPosition();
         },
-        methods:{
+        methods: {
             //自身位置
-            getOwnerPosition(){
+            getOwnerPosition() {
+                let me = this;
                 request.sendGet({
-                    url:"/wx/pay/signature",
-                    params:{
-                        url:location.href
+                    url: "/wx/pay/signature",
+                    params: {
+                        url: location.href
                     }
-                }).then(res =>{
-                    if(res.data.code === 0){
+                }).then(res => {
+                    if (res.data.code === 0) {
                         var data = res.data.data;
                         wx.config({
                             beta: true,// 必须这么写，否则在微信插件有些jsapi会有问题
                             appId: data.appId, // 必填，企业号的唯一标识，此处填写企业号corpid
-                            timestamp: parseInt(data.timestamp,10), // 必填，生成签名的时间戳
+                            timestamp: parseInt(data.timestamp, 10), // 必填，生成签名的时间戳
                             nonceStr: data.nonceStr, // 必填，生成签名的随机串
                             signature: data.signature,// 必填，签名，见附录1
-                            jsApiList: ['getLocation','openLocation'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+                            jsApiList: ['getLocation', 'openLocation'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
                         });
-                        wx.ready(function(){
+                        wx.ready(function () {
                             wx.checkJsApi({
                                 jsApiList: [
                                     'getLocation'
@@ -175,36 +124,51 @@
                             });
                             wx.getLocation({
                                 success: function (res) {
-                                    this.passengerPosition.lat = res.latitude;
-                                    this.passengerPosition.lon = res.longitude;
+                                    // me.passengerPosition.lat = res.latitude;
+                                    // me.passengerPosition.lon = res.longitude;
+                                    me.addMarker(res,'当前位置');
+                                    me.center =  [ res.latitude, res.longitude];
                                 },
-                                fail: function(error) {
+                                fail: function (error) {
                                     this.$toast.fail("获取地理位置失败，请确保开启GPS且允许微信获取您的地理位置！");
                                 }
                             });
                         });
-                        wx.error(function(res){
+                        wx.error(function (res) {
                             console.log(res);
                         });
                     }
                 })
-
+            },
+            addMarker(position,type) {
+                let text= {
+                    position:[position.latitude,position.longitude],
+                    text:type,
+                    offset: [0, 15],
+                };
+                let marker = {
+                    position: [position.latitude,position.longitude],
+                    visible: true,
+                    draggable: false,
+                };
+                this.markers.push(marker);
+                this.texts.push(text);
             },
 
             //获取车主位置
-            getPosition(){
+            getPosition() {
                 request.sendGet({
-                    url:'/sharecar/pass/driver/'+this.tripId,
-                    params:{}
-                }).then(res=>{
-                    if(res.data.code===0){
-                        console.log(res.data.data);
-                    }else{
+                    url: '/sharecar/pass/driver/' + this.tripId,
+                    params: {}
+                }).then(res => {
+                    if (res.data.code === 0) {
+                        this.addMarker(res.data.data,'车主位置');
+                    } else {
                         this.$toast.fail(res.data.msg)
                     }
                 });
             },
-           // getAddress(){},
+            // getAddress(){},
 
         }
     }
@@ -212,10 +176,11 @@
 
 
 <style scoped>
-    .contain{
+    .contain {
         width: 100%;
     }
-    .footer{
+
+    .footer {
         width: 88%;
         margin: 0 auto 55px;
         padding: 15px 10px;
@@ -223,7 +188,8 @@
         font-size: 14px;
         color: #202020;
     }
-    .amap-wrapper{
+
+    .amap-wrapper {
         width: 100%;
         height: 450px;
     }
