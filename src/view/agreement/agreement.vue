5e051d16ab2efdd529aa615ac27c3c49f95f9f10
朝暮 <zhaomu0248@163.com>
2020-02-01 23:22:59 +0800

    协议解码

--- src/view/agreement/agreement.vue
+++ src/view/agreement/agreement.vue
@@ -1,54 +1,47 @@
 <template>
-    <div>
-        <div>
-            <van-nav-bar :fixed="true" :title="title" left-arrow @click-left="onClickLeft"/>
-            <div v-html="content" style="width: 88%;margin:46px auto 0;background: #FFFFFF;padding: 15px 10px"></div>
-        </div>
+    <div style="background-color: #fff">
+            <div v-html="title" style="text-align: center;line-height: 45px;font-size: 20px;"></div>
+            <div v-html="content" class="content"></div>
     </div>
 </template>
 
 <script>
-    import {NavBar} from 'vant';
     import request from '../../utils/request'
 
     export default {
-        components: {
-            [NavBar.name]: NavBar,
-        },
-        data(){
-            return{
-                content:"",
-                title:""
+        data() {
+            return {
+                content: "",
+                title: ""
             }
         },
-        mounted(){
+        mounted() {
             this.initData()
         },
         methods: {
 
-            initData(){
+            initData() {
                 let id = this.$route.query.id;
                 request.sendGet({
-                    url:"/common/clause/"+id,
-                    params:{}
-                }).then(res =>{
-                    if(res.data.code == '0'){
-                        this.content = res.data.data.context;
+                    url: "/common/clause/" + id,
+                    params: {}
+                }).then(res => {
+                    if (res.data.code === 0) {
+                        this.content = this.decode(res.data.data.context);
                         this.title = res.data.data.title
                     }
                 })
             },
-
-            onClickLeft() {
-                this.$router.back(-1);
-            },
+            decode(html) {
+                var temp = document.createElement("div");
+                temp.innerHTML = html;
+                var output = temp.innerText || temp.textContent;
+                temp = null;
+                return output;
+            }
         }
     }
 </script>
 
 <style scoped>
-    /deep/ .van-nav-bar .van-icon {
-        color: #5E5E5E
-    }
-
 </style>
