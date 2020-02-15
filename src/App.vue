01454ae088e4406cce36ddeaed138a7e5e72eff8
朝暮 <zhaomu0248@163.com>
2020-02-15 17:51:28 +0800

    增加是否登录授权判断

--- src/App.vue
+++ src/App.vue
@@ -41,53 +41,57 @@
         },
         methods: {
             getWxConfig() {
-                request.sendGet({
-                    url: "/wx/pay/signature",
-                    params: {
-                        url: location.href
-                    }
-                }).then(res => {
-                    if (res.data.code === 0) {
-                        var data = res.data.data;
-                        wx.config({
-                            beta: true,// 必须这么写，否则在微信插件有些jsapi会有问题
-                            appId: data.appId, // 必填，企业号的唯一标识，此处填写企业号corpid
-                            timestamp: parseInt(data.timestamp, 10), // 必填，生成签名的时间戳
-                            nonceStr: data.nonceStr, // 必填，生成签名的随机串
-                            signature: data.signature,// 必填，签名，见附录1
-                            jsApiList: ['getLocation', 'openLocation'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-                        });
-                    }
-                })
+                if (localStorage.getItem('openid') && localStorage.getItem("isLogin") === '1') {
+                    request.sendGet({
+                        url: "/wx/pay/signature",
+                        params: {
+                            url: location.href
+                        }
+                    }).then(res => {
+                        if (res.data.code === 0) {
+                            var data = res.data.data;
+                            wx.config({
+                                beta: true,// 必须这么写，否则在微信插件有些jsapi会有问题
+                                appId: data.appId, // 必填，企业号的唯一标识，此处填写企业号corpid
+                                timestamp: parseInt(data.timestamp, 10), // 必填，生成签名的时间戳
+                                nonceStr: data.nonceStr, // 必填，生成签名的随机串
+                                signature: data.signature,// 必填，签名，见附录1
+                                jsApiList: ['getLocation', 'openLocation'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+                            });
+                        }
+                    })
+                }
             },
             //自身位置
             updateDriverPosition(tripId) {
-                console.log("调用上传位置了"+tripId);
-                let me = this;
-                wx.ready(function () {
-                    wx.checkJsApi({
-                        jsApiList: [
-                            'getLocation'
-                        ]
+                if (localStorage.getItem('openid') && localStorage.getItem("isLogin") === '1') {
+                    let me = this;
+                    wx.ready(function () {
+                        wx.checkJsApi({
+                            jsApiList: [
+                                'getLocation'
+                            ]
+                        });
+                        wx.getLocation({
+                            success: function (res) {
+                                request.sendPost({
+                                    url: '/sharecar/trip/uplocation/' + tripId,
+                                    params: {
+                                        latitude: res.latitude,
+                                        longitude: res.longitude,
+                                    }
+                                });
+                            },
+                            fail: function (error) {
+                                this.$toast.fail("获取地理位置失败，请确保开启GPS且允许微信获取您的地理位置！");
+                            }
+                        });
                     });
-                    wx.getLocation({
-                        success: function (res) {
-                            request.sendPost({
-                                url: '/sharecar/trip/uplocation/' + tripId,
-                                params: {
-                                    latitude: res.latitude,
-                                    longitude: res.longitude,
-                                }
-                            });
-                        },
-                        fail: function (error) {
-                            this.$toast.fail("获取地理位置失败，请确保开启GPS且允许微信获取您的地理位置！");
-                        }
+                    wx.error(function (res) {
+                        console.log(res);
                     });
-                });
-                wx.error(function (res) {
-                    console.log(res);
-                });
+                }
+
             },
         },
         created() {
